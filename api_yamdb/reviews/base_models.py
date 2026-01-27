10a02e65930d9768b7b0e09d55819a7697c07d38
unknown <an.mezer@yandex.ru>
2026-01-28 05:58:21 +0700

    fix(models): changed the slug field type to SlugField

--- api_yamdb/reviews/base_models.py
+++ api_yamdb/reviews/base_models.py
@@ -6,7 +6,7 @@ from constants.constants import (CHAR_FIELD_LENGTH, SLUG_FIELD_LENGTH,
 
 class NameSlugBaseModel(models.Model):
     name = models.CharField('Название', max_length=CHAR_FIELD_LENGTH)
-    slug = models.CharField(
+    slug = models.SlugField(
         max_length=SLUG_FIELD_LENGTH,
         unique=True)
 
