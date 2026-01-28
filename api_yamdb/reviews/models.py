e83c8ebf0f53835a349d22f4b8202de37efefd64
Mamakov Aleksej <@lexamama92>
2026-01-28 15:48:21 +0300

    add permissions, edit user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,10 +1,26 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from constants.constants import USERS_ROLES
 
 
 class User(AbstractUser):
-    pass
-    # bio = models.TextField()
-    # role = models.Choices(s)
+    email = models.EmailField(unique=True)
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField(
+        'Роль',
+        choices=USERS_ROLES,
+        default=USERS_ROLES[0],
+        blank=True)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if username.lower == 'me':
+            raise ValidationError('Данное имя пользователя запрещено')
+
+        return username
+
+    def __str__(self):
+        return self.username
