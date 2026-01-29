bf24bffc70f2822ac6d75838a35c32093ed19c86
Andrei Mezer <an.mezer@yandex.ru>
2026-01-30 06:08:47 +0700

    Revert "Изменил юзер модель, пермишн"

--- api_yamdb/constants/constants.py
+++ api_yamdb/constants/constants.py
@@ -1,11 +1,9 @@
-from django.conf import settings
-
 FIRST_API_VERSION = 'v1'
-USERS_ROLES = {
-    settings.USER_ROLE: 'user',
-    settings.MODERATOR_ROLE: 'moderator',
-    settings.ADMIN_ROLE: 'admin'
-}
+USERS_ROLES = [
+    (1, 'user'),
+    (2, 'moderator'),
+    (3, 'admin')
+]
 CHAR_FIELD_LENGTH = 256
 SLUG_FIELD_LENGTH = 50
 REGEX_STAMP = '^[-a-zA-Z0-9_]+$'
